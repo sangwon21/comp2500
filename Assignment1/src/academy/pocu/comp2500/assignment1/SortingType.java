e597da93db348864c0ab856e25b35b739cf24832
sangwon <junothyme@gmail.com>
2022-05-04 20:54:29 +0900

    feat: 기본 SortingType 작성

---
+++ Assignment1/src/academy/pocu/comp2500/assignment1/SortingType.java
@@ -0,0 +1,9 @@
+package academy.pocu.comp2500.assignment1;
+
+public enum SortingType {
+    WRITTEN_ASCENDING,
+    WRITTEN_DESCENDING,
+    REVISION_ASCENDING,
+    REVISION_DESCENDING,
+    TITLE_ORDER
+}

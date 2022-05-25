49d4544240e20920707c5ec3b348970054168690
sangwon <junothyme@gmail.com>
2022-05-25 22:26:04 +0900

    feat: Calendar 클래스 작성

---
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Calendar.java
@@ -0,0 +1,26 @@
+package academy.pocu.comp2500.assignment2;
+
+public class Calendar extends Product {
+    private CalendarColor calendarColor;
+
+    public Calendar(final CalendarType calendarType) {
+        this.calendarColor = CalendarColor.WHITE;
+        switch (calendarType) {
+            case DESK:
+                this.setHeight(150);
+                this.setWidth(200);
+                this.setPrice(1000);
+                break;
+            case WALL:
+                this.setHeight(400);
+                this.setWidth(400);
+                this.setPrice(1000);
+                break;
+            case MAGNET:
+                this.setHeight(200);
+                this.setWidth(100);
+                this.setPrice(1500);
+                break;
+        }
+    }
+}

9b9004642b23700d87f68db6b4993c7db47efb15
sangwon <junothyme@gmail.com>
2022-05-26 08:53:21 +0900

    feat: BusinessCard 클래스 추가

---
+++ Assignment2/src/academy/pocu/comp2500/BusinessCardColor.java
@@ -0,0 +1,7 @@
+package academy.pocu.comp2500;
+
+public enum BusinessCardColor {
+    GRAY,
+    IVORY,
+    WHITE
+}
--- Assignment2/src/academy/pocu/comp2500/assignment2/BusinessCard.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BusinessCard.java
@@ -1,4 +1,26 @@
 package academy.pocu.comp2500.assignment2;
 
+import academy.pocu.comp2500.BusinessCardColor;
+
 public class BusinessCard extends Product {
+    private BusinessCardColor businessCardColor;
+
+    public BusinessCard(BusinessCardType businessCardType, BusinessCardSide businessCardSide, BusinessCardColor businessCardColor) {
+        setPrice(100);
+        if (businessCardSide == BusinessCardSide.DOUBLE) {
+            setPrice(this.price + 30);
+        }
+
+        if (businessCardType == BusinessCardType.LINEN) {
+            setPrice(this.price + 10);
+        }
+
+        if (businessCardType == BusinessCardType.LAID) {
+            setPrice(this.price + 20);
+        }
+
+        this.businessCardColor = businessCardColor;
+        setWidth(900);
+        setHeight(500);
+    }
 }

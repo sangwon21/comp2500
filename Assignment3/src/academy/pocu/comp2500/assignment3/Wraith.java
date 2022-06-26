607cf0b54f472a29b462106501778f7347519913
sangwon <junothyme@gmail.com>
2022-06-27 00:30:30 +0900

    feat: Wraith 작성

--- Assignment3/src/academy/pocu/comp2500/assignment3/Wraith.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/Wraith.java
@@ -1,5 +1,250 @@
 package academy.pocu.comp2500.assignment3;
 
-public class Wraith extends Unit {
+import java.util.HashSet;
 
+public class Wraith extends Unit implements IMovable, IThinkable {
+    private static final char SYMBOL = 'W';
+    private static final int VISION = 4;
+    private static final int AREA_OF_EFFECT = 0;
+    private static final int AP = 6;
+    private static final int HP = 88;
+    private static final EUnitType[] POSSIBLE_ATTACK_UNIT_TYPES = {EUnitType.GROUND, EUnitType.AIR};
+    private static final IntVector2D[] ATTACK_AREA_RANGE = {
+            new IntVector2D(0, 0),
+            new IntVector2D(0, -1),
+            new IntVector2D(1, 0),
+            new IntVector2D(0, 1),
+            new IntVector2D(-1, 0)
+    };
+    private static final IntVector2D[] VISION_OFFSETS = getVisionOffsets(VISION);
+    private IntVector2D originalPosition;
+
+    private Unit targetOrNull;
+    private boolean hasShield;
+
+    public Wraith(IntVector2D position) {
+        super(position, HP, SYMBOL, EUnitType.AIR);
+        this.hasShield = true;
+        this.originalPosition = position;
+    }
+
+    public void onSpawn() {
+        SimulationManager.getInstance().registerThinkable(this);
+        SimulationManager.getInstance().registerMovable(this);
+    }
+
+    public void think() {
+        this.targetOrNull = findAttackTargetOrNull();
+        if (this.targetOrNull != null) {
+            this.action = EActionType.ATTACK;
+            return;
+        }
+
+        this.action = EActionType.MOVE;
+        this.targetOrNull = findNextMovePosition();
+    }
+
+    private void moveByOneUnit(int targetY, int targetX) {
+        BattleField battleField = SimulationManager.getInstance().getBattleField();
+
+        int toY = this.position.getY();
+        int toX = this.position.getX();
+
+        if (targetY != this.position.getY()) {
+            if (this.position.getY() < targetY) {
+                toY += 1;
+            } else {
+                toY -= 1;
+            }
+        } else if (targetX != this.position.getX()) {
+            if (this.position.getX() < targetX) {
+                toX += 1;
+            } else {
+                toX -= 1;
+            }
+        }
+
+        battleField.move(this.position.getY(), this.position.getX(), toY, toX, this);
+        this.position.setX(toX);
+        this.position.setY(toY);
+    }
+
+    private void moveToTarget() {
+        IntVector2D targetOrNullPosition = this.targetOrNull.getPosition();
+
+        int targetY = targetOrNullPosition.getY();
+        int targetX = targetOrNullPosition.getX();
+
+        moveByOneUnit(targetY, targetX);
+    }
+
+    private void moveToOriginalPosition() {
+        int targetY = this.originalPosition.getY();
+        int targetX = this.originalPosition.getX();
+
+        moveByOneUnit(targetY, targetX);
+    }
+
+    public void move() {
+        if (this.action != EActionType.MOVE) {
+            return;
+        }
+
+        if (this.targetOrNull == null) {
+            moveToOriginalPosition();
+            return;
+        }
+
+        moveToTarget();
+    }
+
+    public AttackIntent attack() {
+        if (this.action != EActionType.ATTACK) {
+            return null;
+        }
+
+        return new AttackIntent(this, 1, 1, AP, AREA_OF_EFFECT, POSSIBLE_ATTACK_UNIT_TYPES);
+    }
+
+    @Override
+    public void onAttacked(int damage) {
+        if (this.hasShield) {
+            this.hasShield = false;
+            return;
+        }
+
+        this.hp -= damage;
+    }
+
+    private Unit findAttackTargetOrNull() {
+        BattleField battleField = SimulationManager.getInstance().getBattleField();
+        Unit airTargetOrNull = null;
+        Unit groundTargetOrNull = null;
+
+        for (IntVector2D offset : ATTACK_AREA_RANGE) {
+            int offsetY = offset.getY();
+            int offsetX = offset.getX();
+
+            int targetY = offsetY + position.getY();
+            int targetX = offsetX + position.getX();
+
+            if (battleField.isValidPosition(targetY, targetX) == false) {
+                continue;
+            }
+
+            HashSet<Unit> unitSet = battleField.getUnitsFromPosition(targetY, targetX);
+
+            for (Unit unit : unitSet) {
+                if (unit == this) {
+                    continue;
+                }
+
+                if (unit.unitType == EUnitType.AIR) {
+                    if (airTargetOrNull == null) {
+                        airTargetOrNull = unit;
+                        continue;
+                    }
+
+                    if (airTargetOrNull.getHp() > unit.getHp()) {
+                        airTargetOrNull = unit;
+                        continue;
+                    }
+                }
+
+                if (groundTargetOrNull == null) {
+                    groundTargetOrNull = unit;
+                    continue;
+                }
+
+                if (groundTargetOrNull.getHp() > unit.getHp()) {
+                    groundTargetOrNull = unit;
+                }
+            }
+        }
+
+        if (airTargetOrNull != null) {
+            return airTargetOrNull;
+        }
+
+        return groundTargetOrNull;
+    }
+
+    private Unit findNextMovePosition() {
+        BattleField battleField = SimulationManager.getInstance().getBattleField();
+        Unit airTargetOrNull = null;
+        int airMinDistance = Integer.MAX_VALUE;
+        Unit groundTargetOrNull = null;
+        int groundMinDistance = Integer.MAX_VALUE;
+
+        for (IntVector2D offset : VISION_OFFSETS) {
+            int offsetY = offset.getY();
+            int offsetX = offset.getX();
+
+            int targetY = offsetY + position.getY();
+            int targetX = offsetX + position.getX();
+
+            if (battleField.isValidPosition(targetY, targetX) == false) {
+                continue;
+            }
+
+            HashSet<Unit> unitSet = battleField.getUnitsFromPosition(targetY, targetX);
+
+            for (Unit unit : unitSet) {
+                if (unit == this) {
+                    continue;
+                }
+
+                int distance = getDistanceFrom(unit);
+
+                if (unit.unitType == EUnitType.AIR) {
+                    if (airTargetOrNull == null) {
+                        airTargetOrNull = unit;
+                        airMinDistance = distance;
+                        continue;
+                    }
+
+                    if (airMinDistance < distance) {
+                        continue;
+                    }
+
+                    if (airMinDistance > distance) {
+                        airTargetOrNull = unit;
+                        airMinDistance = distance;
+                        continue;
+                    }
+
+                    if (airTargetOrNull.getHp() > unit.getHp()) {
+                        airTargetOrNull = unit;
+                        continue;
+                    }
+                }
+
+                if (groundTargetOrNull == null) {
+                    groundTargetOrNull = unit;
+                    groundMinDistance = distance;
+                    continue;
+                }
+
+                if (groundMinDistance < distance) {
+                    continue;
+                }
+
+                if (groundMinDistance > distance) {
+                    groundTargetOrNull = unit;
+                    groundMinDistance = distance;
+                    continue;
+                }
+
+                if (groundTargetOrNull.getHp() > unit.getHp()) {
+                    groundTargetOrNull = unit;
+                }
+            }
+        }
+
+        if (airTargetOrNull != null) {
+            return airTargetOrNull;
+        }
+
+        return groundTargetOrNull;
+    }
 }

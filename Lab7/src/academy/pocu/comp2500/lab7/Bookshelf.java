a6a4a8edc57d6330a855311b4cc8e8d1bc69094b
sangwon <junothyme@gmail.com>
2022-06-27 21:50:12 +0900

    feat: Bookshelf 클래스 작성

---
+++ Lab7/src/academy/pocu/comp2500/lab7/Bookshelf.java
@@ -0,0 +1,28 @@
+package academy.pocu.comp2500.lab7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Bookshelf {
+    private List<Book> books;
+    private int maxBookCapacity;
+
+    public Bookshelf(final int maxBookCapacity) {
+        this.maxBookCapacity = maxBookCapacity;
+        this.books = new ArrayList<>(this.maxBookCapacity);
+    }
+
+    public boolean add(final Book book) {
+        if (this.books.size() >= this.maxBookCapacity) {
+            return false;
+        }
+
+        this.books.add(book);
+        return true;
+    }
+
+    public boolean remove(final Book book) {
+        return this.books.remove(book);
+    }
+}

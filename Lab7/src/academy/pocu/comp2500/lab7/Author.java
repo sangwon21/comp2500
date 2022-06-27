d55952669717fa8138216cc3b661cd0c7a27ecb0
sangwon <junothyme@gmail.com>
2022-06-27 21:34:08 +0900

    feat: Author 클래스 작성

---
+++ Lab7/src/academy/pocu/comp2500/lab7/Author.java
@@ -0,0 +1,34 @@
+package academy.pocu.comp2500.lab7;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(final String firstName, final String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || obj.hashCode() != this.hashCode()) {
+            return false;
+        }
+
+        return ((Author)obj).firstName.equals(this.firstName) && ((Author) obj).lastName.equals(this.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.firstName.hashCode() ^ (this.lastName.hashCode() << 16);
+    }
+}

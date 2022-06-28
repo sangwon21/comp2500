9c3338f9acb252aeaad428464a9a5567a6490835
sangwon <junothyme@gmail.com>
2022-06-29 00:09:39 +0900

    feat: Symbol 클래스 추가

---
+++ Assignment3/src/academy/pocu/comp2500/Symbol.java
@@ -0,0 +1,11 @@
+package academy.pocu.comp2500;
+
+public class Symbol {
+    public static final char Marine = 'M';
+    public static final char Tank = 'T';
+    public static final char Wraith = 'W';
+    public static final char Turret = 'U';
+    public static final char Mine = 'N';
+    public static final char SmartMine = 'A';
+    public static final char Destroyer = 'D';
+}

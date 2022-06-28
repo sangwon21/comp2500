00726e990a02b82cdee695ffce7d281a0ec83caa
sangwon <junothyme@gmail.com>
2022-06-29 00:32:25 +0900

    feat: Destroyer 클래스 추가

--- Assignment3/src/academy/pocu/comp2500/assignment3/Destroyer.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/Destroyer.java
@@ -1,4 +1,35 @@
 package academy.pocu.comp2500.assignment3;
 
+import academy.pocu.comp2500.Symbol;
+
 public class Destroyer extends Unit {
+    private static final EUnitType UNIT_TYPE = EUnitType.AIR;
+    private static final int AREA_OF_EFFECT = 16;
+    private static final int AP = 100000;
+    private static final int HP = 100000;
+    private static final EUnitType[] POSSIBLE_ATTACK_UNIT_TYPES = {EUnitType.GROUND, EUnitType.AIR, EUnitType.INVISIBLE};
+
+
+    public Destroyer(final IntVector2D position) {
+        super(position, HP, Symbol.Destroyer, UNIT_TYPE);
+    }
+
+    @Override
+    public void onSpawn() {
+    }
+
+    @Override
+    public void onRemove() {
+    }
+
+    @Override
+    public AttackIntent attack() {
+
+        return new AttackIntent(this, this.position.getY(), this.position.getX(), AP, AREA_OF_EFFECT, POSSIBLE_ATTACK_UNIT_TYPES, false);
+    }
+
+    @Override
+    public void onAttacked(int damage) {
+        this.hp -= 1;
+    }
 }

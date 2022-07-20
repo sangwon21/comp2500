5b443764800a2c1b4b157bad80be8ed0f57d4d66
sangwon <junothyme@gmail.com>
2022-07-20 21:59:14 +0900

    feat: Request 클래스 작성

---
+++ Lab10/src/academy/pocu/comp2500/lab10/Request.java
@@ -0,0 +1,17 @@
+package academy.pocu.comp2500.lab10;
+
+import academy.pocu.comp2500.lab10.pocuflix.User;
+
+public class Request {
+    private final String title;
+    private User user;
+
+    public Request(String title) {
+        this.title = title;
+        this.user = new User("", "");
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

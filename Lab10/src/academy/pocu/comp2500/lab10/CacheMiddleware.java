8bd8d87bd4addfd089ee934af6ec9cdb155408a8
sangwon <junothyme@gmail.com>
2022-07-22 15:02:40 +0900

    feat: CacheMiddleware 작성

---
+++ Lab10/src/academy/pocu/comp2500/lab10/CacheMiddleware.java
@@ -0,0 +1,43 @@
+package academy.pocu.comp2500.lab10;
+
+import academy.pocu.comp2500.lab10.pocuflix.ResultBase;
+import academy.pocu.comp2500.lab10.pocuflix.ResultCode;
+
+import java.util.HashMap;
+
+public class CacheMiddleware implements IRequestHandler {
+    private IRequestHandler requestHandler;
+    private HashMap<Request, Integer> cacheUsedCountMap;
+    private int expiryCount;
+
+    public CacheMiddleware(IRequestHandler requestHandler, int expiryCount) {
+        this.requestHandler = requestHandler;
+        this.cacheUsedCountMap = new HashMap<>();
+        this.expiryCount = expiryCount;
+    }
+
+
+    @Override
+    public ResultBase handle(Request request) {
+        if (this.cacheUsedCountMap.containsKey(request)) {
+            int usedCount = this.cacheUsedCountMap.get(request) + 1;
+
+
+            if (usedCount < this.expiryCount) {
+                this.cacheUsedCountMap.put(request, usedCount);
+                return new CachedResult(this.expiryCount - usedCount);
+            }
+
+            this.cacheUsedCountMap.remove(request);
+        }
+
+        ResultBase resultBase = this.requestHandler.handle(request);
+        ResultValidator validator = new ResultValidator(resultBase);
+
+        if (validator.isValid(ResultCode.OK)) {
+            this.cacheUsedCountMap.put(request, 0);
+        }
+
+        return resultBase;
+    }
+}

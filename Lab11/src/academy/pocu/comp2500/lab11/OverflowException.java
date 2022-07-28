b99b62c26aa01a163bd7fc4a14daeb616385f734
sangwon <junothyme@gmail.com>
2022-07-28 11:38:15 +0900

    fix: serialVersionUID 추가

--- Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
+++ Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
@@ -1,6 +1,8 @@
 package academy.pocu.comp2500.lab11;
 
 public class OverflowException extends RuntimeException {
+    private static final long serialVersionUID = -6184044926029705156L;
+
     public OverflowException(String msg) {
         super(msg);
     }

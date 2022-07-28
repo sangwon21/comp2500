4079961fd6a2f5f2ab2647cba2e7109332911ae0
sangwon <junothyme@gmail.com>
2022-07-28 13:31:31 +0900

    feat: 프로그램에 코드 추가

--- Lab11/src/academy/pocu/comp2500/lab11/SafeWallet.java
+++ Lab11/src/academy/pocu/comp2500/lab11/SafeWallet.java
@@ -4,11 +4,11 @@ import academy.pocu.comp2500.lab11.pocu.User;
 import academy.pocu.comp2500.lab11.pocu.Wallet;
 
 public final class SafeWallet extends Wallet {
-    public SafeWallet(final User user) throws IllegalAccessException {
+    public SafeWallet(User user) throws IllegalAccessException {
         super(user);
     }
 
-    public boolean deposit(final int amount) {
+    public boolean deposit(int amount) {
         if (getAmount() > 0 && getAmount() + amount <= 0) {
             throw new OverflowException("Overflow");
         }
--- Lab11/src/academy/pocu/comp2500/lab11/app/Program.java
+++ Lab11/src/academy/pocu/comp2500/lab11/app/Program.java
@@ -1,8 +1,16 @@
 package academy.pocu.comp2500.lab11.app;
 
+import academy.pocu.comp2500.lab11.App;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Program {
 
     public static void main(String[] args) {
         // write your code here
+        App app = new App();
+
+        app.run(new BufferedReader(new InputStreamReader(System.in)), System.out, System.err);
     }
 }
